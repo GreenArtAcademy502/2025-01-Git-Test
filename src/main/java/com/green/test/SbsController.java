bacebd825cc955fed4a63d764a2262f3db76a505
DoheumPark <pirbak@hanmail.net>
2025-07-14 15:32:18 +0900

    sbs getter

--- src/main/java/com/green/test/SbsController.java
+++ src/main/java/com/green/test/SbsController.java
@@ -1,7 +1,15 @@
 package com.green.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sbs")
 public class SbsController {
+
+    @GetMapping
+    public String index() {
+        return "sbs!!";
+    }
 }
